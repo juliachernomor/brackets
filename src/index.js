fafc03c1d91eaa03b8f36b47bbc0816682c85a49
Iuliia Chernomorskaia <yukka.spb@gmail.com>
2024-08-11 05:47:14 +0300

    feat: 17/20

--- src/index.js
+++ src/index.js
@@ -1,24 +1,31 @@
 
-module.exports = function check(str, bracketsConfig) {
-  let stack = [];
-  let obj = Object.fromEntries(bracketsConfig);
-  for (let char of str) {
-    if ((Object.keys(obj).join('')).localeCompare(char) == 0) {
-      stack.push(char)
-    } else if ((Object.values(obj).join('')).localeCompare(char) == 0) {
-        if (stack.length == 0 || stack.pop() !== getKeyByValue(obj, char)) {
-        return false;
-      }
+
+  function getKeyByValue(object, value) {//получаю ключ по значению
+    return Object.keys(object).find(key =>
+        object[key] === value);
     }
-  }
-  if (stack.length == 0) {
-    return true
-}
-}
 
+module.exports = function check(str, bracketsConfig) {
+    let stack = [];
+    let obj = Object.fromEntries(bracketsConfig);
+    for (let char of str) {
 
-function getKeyByValue(object, value) {//получаю ключ по значению
-  return Object.keys(object).find(key =>
-      object[key] === value);
+        for (let key in obj) {
+            if (key.localeCompare(char) == 0) {
+                stack.push(char)
+            }
+        }
+
+        for (let key in obj) {
+            if (obj[key].localeCompare(char) == 0) {
+                if (stack.length == 0 || stack.pop() !== getKeyByValue(obj, char)) {
+                    return false;
+                }
+            }
+        }
+    }
+    if (stack.length == 0) {
+        return true
+    } else { 
+        return false }
 }
-
