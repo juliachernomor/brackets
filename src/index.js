691a539284a689ddf6a999c795fdf91237d61b5d
Iuliia Chernomorskaia <yukka.spb@gmail.com>
2024-08-11 04:39:11 +0300

    feat: 11/20

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let obj = Object.fromEntries(bracketsConfig);
+  for (let char of str) {
+    if ((Object.keys(obj).join('')).localeCompare(char) == 0) {
+      stack.push(char)
+    } else if ((Object.values(obj).join('')).localeCompare(char) == 0) {
+        if (stack.length == 0 || stack.pop() !== getKeyByValue(obj, char)) {
+        return false;
+      }
+    }
+  }
+  if (stack.length == 0) {
+    return true
 }
+}
+
+
+function getKeyByValue(object, value) {//получаю ключ по значению
+  return Object.keys(object).find(key =>
+      object[key] === value);
+}
+
