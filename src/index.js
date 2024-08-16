26564ddcc7d7476b4d0c000badb3f7c0c9acd0bd
Iuliia Chernomorskaia <yukka.spb@gmail.com>
2024-08-16 19:31:52 +0300

    fix: deletу comments

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// 1
 // module.exports = function check(str, bracketsConfig) {
 //     const getPairs = []
 //     bracketsConfig.forEach((bracket) => getPairs.push(bracket[0] + bracket[1]))
@@ -8,29 +9,8 @@
 //     }
 //     return str? false:true 
 // }
-
+// 2
 module.exports = function check(str, bracketsConfig) {
-    // let stack = [];
-    // for (let char of str) {
-    //     bracketsConfig.forEach((mass) => {
-    //         //символ строки равен открывающим скобкам
-    //         if (mass[0] === char && mass[1] !==char) {
-    //             stack.push(char);
-    //         }   //символ строки равен  скобкам одинаковым у откр и закр
-    //         else if(mass[1] === char && mass[0] === char){ 
-    //             if (stack.length ==0 || stack[stack.length - 1] !== char) 
-    //             { stack.push(char);}
-    //             else if (stack.length !==0 && stack[stack.length - 1] == char) {
-    //             stack.pop(char);
-    //             }
-    //         }//символ строки равен закрывающим скобкам
-    //         else if (mass[1] === char && mass[0] !== char) {
-    //             if (!stack.length || stack[stack.length - 1] !== mass[0]) return false;
-    //             stack.pop(char);
-    //         } 
-    //     });
-    // }
-    // return !stack.length;
     let stack = [];
     for (let char of str) {
         for (let mass of bracketsConfig) {
